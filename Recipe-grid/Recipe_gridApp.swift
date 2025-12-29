db87246f0583f829d37c82116cbcce03d6d85ff8
NAstyasasasas <142779148+NAstyasasasas@users.noreply.github.com>
2025-12-29 16:38:04 +0300

    Initial Commit

---
+++ Recipe-grid/Recipe_gridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_gridApp.swift
+//  Recipe-grid
+//
+//  Created by Анастасия on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_gridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
